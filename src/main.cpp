a15a74318fb26b3d2287af63650af262e6b4cba4
edmondburnett <_@edmondburnett.com>
2022-05-01 10:01:15 -0700

    add structop

--- src/main.cpp
+++ src/main.cpp
@@ -2,32 +2,82 @@
 #include <string>
 #include <vector>
 
-#include <argparse.hpp>
+// #include <argparse.hpp>
+#include <structop.hpp>
 
 using namespace std;
 
-struct MyArgs : public argparse::Args
+struct Options
 {
-    std::string &command = arg("command", "One of: init, build");
-    // std::string &source = arg("source", "source path");
-    // int &k = kwarg("k", "A keyworded integer value");
-    // float &alpha = kwarg("a,alpha", "An optional float value").set_default(0.5f);
-    // bool &verbose = flag("v,verbose", "A flag to toggle verbose");
-    // bool &dryrun = flag("d,dry-run", "Perform a dry run");
+    // positional argument
+    //   e.g., ./main <file>
+    enum class Commands
+    {
+        init,
+        build,
+    };
+    Commands command;
+    // std::string command;
+
+    // optional argument
+    //   e.g., -b "192.168.5.3"
+    //   e.g., --bind_address "192.168.5.3"
+    //
+    // options can be delimited with `=` or `:`
+    // note: single dash (`-`) is enough for short & long option
+    //   e.g., -bind_address=localhost
+    //   e.g., -b:192.168.5.3
+    //
+    // the long option can also be provided in kebab case:
+    //   e.g., --bind-address 192.168.5.3
+    // std::optional<std::string> bind_address;
+
+    // flag argument
+    // Use `std::optional<bool>` and provide a default value.
+    //   e.g., -v
+    //   e.g., --verbose
+    //   e.g., -verbose
+    // std::optional<bool> verbose = false;
+
+    // directly define and use enum classes to limit user choice
+    //   e.g., --log-level debug
+    //   e.g., -l error
+    // enum class LogLevel
+    // {
+    //     debug,
+    //     info,
+    //     warn,
+    //     error,
+    //     critical
+    // };
+    // std::optional<LogLevel> log_level = LogLevel::info;
+
+    // pair argument
+    // e.g., -u <first> <second>
+    // e.g., --user <first> <second>
+    // std::optional<std::pair<std::string, std::string> > user;
 };
+STRUCTOPT(Options, command);
 
 int main(int argc, char **argv)
 {
-
-    MyArgs args = argparse::parse<MyArgs>(argc, argv);
-
-    if (args.command == "init")
+    try
     {
-        std::cout << "Initializing new project..." << std::endl;
+        auto options = structopt::app("my_app").parse<Options>(argc, argv);
+
+        if (options.command == Options::Commands::build)
+        {
+            std::cout << "building..." << std::endl;
+        }
+        else if (options.command == Options::Commands::init)
+        {
+            std::cout << "initializing..." << std::endl;
+        }
     }
-    else if (args.command == "build")
+    catch (structopt::exception &e)
     {
-        std::cout << "Building project..." << std::endl;
+        std::cout << e.what() << "\n";
+        std::cout << e.help();
     }
 
     return 0;
