89c07f4d25163776c827d039191637091a04a2a3
edmondburnett <_@edmondburnett.com>
2022-05-01 10:29:54 -0700

    cleanup

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,26 @@
 #include <iostream>
-#include <string>
-#include <vector>
 
-// #include <argparse.hpp>
 #include <structop.hpp>
-
-using namespace std;
 
 struct Options
 {
-    // positional argument
-    //   e.g., ./main <file>
     enum class Commands
     {
         init,
         build,
     };
     Commands command;
-    // std::string command;
 
-    // optional argument
-    //   e.g., -b "192.168.5.3"
-    //   e.g., --bind_address "192.168.5.3"
-    //
-    // options can be delimited with `=` or `:`
-    // note: single dash (`-`) is enough for short & long option
-    //   e.g., -bind_address=localhost
-    //   e.g., -b:192.168.5.3
-    //
-    // the long option can also be provided in kebab case:
-    //   e.g., --bind-address 192.168.5.3
-    // std::optional<std::string> bind_address;
-
-    // flag argument
-    // Use `std::optional<bool>` and provide a default value.
-    //   e.g., -v
-    //   e.g., --verbose
-    //   e.g., -verbose
-    // std::optional<bool> verbose = false;
-
-    // directly define and use enum classes to limit user choice
-    //   e.g., --log-level debug
-    //   e.g., -l error
-    // enum class LogLevel
-    // {
-    //     debug,
-    //     info,
-    //     warn,
-    //     error,
-    //     critical
-    // };
-    // std::optional<LogLevel> log_level = LogLevel::info;
-
-    // pair argument
-    // e.g., -u <first> <second>
-    // e.g., --user <first> <second>
-    // std::optional<std::pair<std::string, std::string> > user;
+    std::optional<bool> quiet = false;
 };
-STRUCTOPT(Options, command);
+STRUCTOPT(Options, command, quiet);
 
 int main(int argc, char **argv)
 {
+    int return_value = 0;
     try
     {
-        auto options = structopt::app("my_app").parse<Options>(argc, argv);
+        auto options = structopt::app("fuligin", "1.0").parse<Options>(argc, argv);
 
         if (options.command == Options::Commands::build)
         {
@@ -78,7 +35,8 @@ int main(int argc, char **argv)
     {
         std::cout << e.what() << "\n";
         std::cout << e.help();
+        return_value = 1;
     }
 
-    return 0;
+    return return_value;
 }
